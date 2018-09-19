753fcee91b5e05f68198bca8de6a1b5adb060a0a
Wolfik <Wolfik@DESKTOP-C3Q0MUB>
2018-09-19 23:41:39 +0200

    Added MySQL database connector. Established connection

---
+++ BlaBlaSGGWService/src/main/java/sggw/blabla/app/BlaBlaSggwServiceApplication.java
@@ -0,0 +1,12 @@
+package sggw.blabla.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlaBlaSggwServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlaBlaSggwServiceApplication.class, args);
+	}
+}
